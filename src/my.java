f3f864e3775b1fc763835c057fe4523172bdc36e
krissloveis <yorkyy@mail.ru>
2024-08-29 15:07:28 +0300

    initial commit 2

--- src/my.java
+++ src/my.java
@@ -3,10 +3,12 @@ public class my {
         System.out.println("Система расчета стоимости топлива:");
 
         int fuelType = 95;
-        int amount = 50;
+        int amount = 420;
 
         double fuel92price = 60.2;
         double fuel95price = 67.33;
+
+        int maxAmount = 400;
 
         double fuelPrice = 0;
         if (fuelType == 92) {
@@ -22,6 +24,11 @@ public class my {
          if (amount < 1) {
              System.out.println("Указано слишком малое количество топлива");
               amount = 0;
+         }
+
+         if (amount > maxAmount) {
+             System.out.println("Указанное количество топлива превышает максимально допустимое");
+             amount = maxAmount;
          }
 
 
